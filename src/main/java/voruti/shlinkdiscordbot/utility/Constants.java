6eddd31ec5ffe3ff76f689a61f42341927164953
voruti <45130096+voruti@users.noreply.github.com>
2021-08-04 18:15:12 +0200

    Remove unused methods, etc.

--- src/main/java/voruti/shlinkdiscordbot/utility/Constants.java
+++ src/main/java/voruti/shlinkdiscordbot/utility/Constants.java
@@ -2,33 +2,11 @@ package voruti.shlinkdiscordbot.utility;
 
 import okhttp3.MediaType;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Class to contain (static) constant values that can be accessed from any other class.
  */
 public final class Constants {
 
-    /**
-     * The prefix used for all bot commands.
-     */
-    public static final String CMD_CHAR = "!";
-    /**
-     * {@link String Strings} the user can use for the "help" command.
-     */
-    public static final List<String> HELP_CMD = Arrays.asList(
-            CMD_CHAR + "was", CMD_CHAR + "what",
-            CMD_CHAR + "hilfe", CMD_CHAR + "help"
-    );
-    /**
-     * {@link String Strings} the user can use for the "add new Shlink" command.
-     */
-    public static final List<String> ADD_SHLINK_CMD = Arrays.asList(
-            CMD_CHAR + "addShlink",
-            CMD_CHAR + "addshlink",
-            CMD_CHAR + "addSL", CMD_CHAR + "addsl"
-    );
     /**
      * Version of Shlink's REST-API that should be used.
      */
--- src/main/java/voruti/shlinkdiscordbot/utility/StaticMethods.java
+++ src/main/java/voruti/shlinkdiscordbot/utility/StaticMethods.java
@@ -2,10 +2,8 @@ package voruti.shlinkdiscordbot.utility;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.dv8tion.jda.api.entities.Message;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,19 +20,6 @@ public final class StaticMethods {
     private StaticMethods() {
     }
 
-
-    /**
-     * Tests {@link Message} if it matches a command list from {@link Constants}.
-     *
-     * @param msg     the {@link Message} to test
-     * @param cmdList a {@link List} of {@link String Strings} that represent permitted commands
-     * @return true, if the {@link Message} matches, false otherwise
-     */
-    public static boolean messageMatchesCmd(Message msg, List<String> cmdList) {
-        final String msgContent = msg.getContentRaw();
-        return cmdList.parallelStream()
-                .anyMatch(msgContent::startsWith);
-    }
 
     /**
      * Creates a JSON {@link String} from a {@link Map}. Only works for primitive types as key and value inside the {@link Map}.
