1fb4917067a3274266cc33cfe121653686043b41
voruti <45130096+voruti@users.noreply.github.com>
2021-08-04 18:06:55 +0200

    Remove old tests for "command by message"

--- src/test/java/voruti/shlinkdiscordbot/ShlinkCreatorTest.java
+++ src/test/java/voruti/shlinkdiscordbot/ShlinkCreatorTest.java
@@ -1,20 +1,14 @@
 package voruti.shlinkdiscordbot;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
-import net.dv8tion.jda.api.requests.restaction.MessageAction;
 import net.dv8tion.jda.api.requests.restaction.WebhookMessageAction;
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction;
-import net.dv8tion.jda.internal.entities.UserById;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,7 +24,6 @@ class ShlinkCreatorTest {
 
     private static ShlinkCreator shlinkCreator;
 
-    private MessageReceivedEvent messageReceivedEvent;
     private SlashCommandEvent slashCommandEvent;
 
 
@@ -45,102 +38,7 @@ class ShlinkCreatorTest {
 
     @BeforeEach
     void setUp() {
-        messageReceivedEvent = mock(MessageReceivedEvent.class);
         slashCommandEvent = mock(SlashCommandEvent.class);
-    }
-
-
-    @Test
-    void onMessageReceived_SelfMessage() {
-        // arrange:
-        User theBotItself = new UserById(5L);
-        when(messageReceivedEvent.getAuthor()).thenReturn(theBotItself);
-
-        // act:
-        shlinkCreator.onMessageReceived(messageReceivedEvent);
-
-        // assert:
-        verify(messageReceivedEvent, never()).getMessage();
-    }
-
-    @Test
-    void onMessageReceived_RandomMessage() {
-        // arrange:
-        User author = new UserById(10L);
-        when(messageReceivedEvent.getAuthor()).thenReturn(author);
-
-        Message message = new MessageBuilder().append("message should be ignored").build();
-        when(messageReceivedEvent.getMessage()).thenReturn(message);
-        MessageChannel channel = mock(MessageChannel.class);
-        when(messageReceivedEvent.getChannel()).thenReturn(channel);
-
-        // act:
-        shlinkCreator.onMessageReceived(messageReceivedEvent);
-
-        // assert:
-        verify(messageReceivedEvent).getMessage();
-        verify(messageReceivedEvent).getChannel();
-        verify(channel, never()).sendMessage(any(CharSequence.class));
-    }
-
-    @Test
-    void onMessageReceived_HelpMessage() {
-        // arrange:
-        User author = new UserById(10L);
-        when(messageReceivedEvent.getAuthor()).thenReturn(author);
-
-        Message message = new MessageBuilder().append("!help").build();
-        when(messageReceivedEvent.getMessage()).thenReturn(message);
-        MessageChannel channel = mock(MessageChannel.class);
-        when(messageReceivedEvent.getChannel()).thenReturn(channel);
-
-        when(channel.sendMessage(any(CharSequence.class))).thenReturn(mock(MessageAction.class));
-
-        // act:
-        shlinkCreator.onMessageReceived(messageReceivedEvent);
-
-        // assert:
-        verify(channel).sendMessage("!addShlink <long URL> [custom slug]\t:\tCreate a short link from <long URL> with optional [custom slug].");
-    }
-
-    @Test
-    void onMessageReceived_AddShlinkMessage() {
-        // arrange:
-        User author = new UserById(10L);
-        when(messageReceivedEvent.getAuthor()).thenReturn(author);
-
-        Message message = new MessageBuilder().append("!addShlink example.com").build();
-        when(messageReceivedEvent.getMessage()).thenReturn(message);
-        MessageChannel channel = mock(MessageChannel.class);
-        when(messageReceivedEvent.getChannel()).thenReturn(channel);
-
-        when(channel.sendMessage(any(CharSequence.class))).thenReturn(mock(MessageAction.class));
-
-        // act:
-        shlinkCreator.onMessageReceived(messageReceivedEvent);
-
-        // assert:
-        verify(channel).sendMessage("Error with Shlink's response: \"null\"!");
-    }
-
-    @Test
-    void onMessageReceived_AddShlinkMessageWCustomSlug() {
-        // arrange:
-        User author = new UserById(10L);
-        when(messageReceivedEvent.getAuthor()).thenReturn(author);
-
-        Message message = new MessageBuilder().append("!addShlink example.com test").build();
-        when(messageReceivedEvent.getMessage()).thenReturn(message);
-        MessageChannel channel = mock(MessageChannel.class);
-        when(messageReceivedEvent.getChannel()).thenReturn(channel);
-
-        when(channel.sendMessage(any(CharSequence.class))).thenReturn(mock(MessageAction.class));
-
-        // act:
-        shlinkCreator.onMessageReceived(messageReceivedEvent);
-
-        // assert:
-        verify(channel).sendMessage("Error with Shlink's response: \"null\"!");
     }
 
 
